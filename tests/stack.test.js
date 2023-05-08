2668f5926f03025881e84c2e5d4c67ba40c89311
simm99 <100690242+simm99@users.noreply.github.com>
2023-05-08 16:05:50 +0200

    test som inte går igenom

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,19 +1,16 @@
-const stack = require('../src/stack');
+const Stack = require('./stack');
 
-test('peek on empty stack returns undefined', () => {
-    expect(stack.peek()).toBeUndefined();
-});
+describe('Stack', () => {
+  let stack;
 
-test('peek on stack with one element returns that element', () => {
-    stack.push(1);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(1);
-});
+  beforeEach(() => {
+    stack = new Stack();
+  });
 
-test('peek on stack with two or more elements returns the top element', () => {
-    stack.push(1);
-    stack.push("wow");
-    stack.push(42);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(42);
+  it('should return the last element pushed to the stack', () => {
+    stack.push('foo');
+    stack.push('bar');
+    stack.push('baz');
+    expect(stack.peek()).toEqual('baz');
+  });
 });
