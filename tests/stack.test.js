36697dd560e2d89f4afdb102236d86dc8f8b7ea4
simm99 <100690242+simm99@users.noreply.github.com>
2023-05-10 09:45:59 +0200

    Började om med testerna

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,13 +1,19 @@
-const Stack = require('./stack');
+const stack = require('../src/stack');
 
-describe('Stack', () => {
-  let stack;
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
 
-  beforeEach(() => {
-    stack = new Stack();
-  });
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
 
-  it('should throw an error when popping from an empty stack', () => {
-    expect(() => stack.pop()).toThrowError('Stack is empty');
-  });
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
 });
